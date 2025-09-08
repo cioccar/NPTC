4404ad254c6e0716190fbbf65266a6002c80a853
cioccar <cioccar@amazon.com>
2025-09-08 17:02:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,8 @@ import pandas as pd
 
 st.title('NPT hours available')
 
-# Sidebar headers with consistent styling
-st.sidebar.markdown("""
-<style>
-    .sidebar-header {
-        font-size: 16px;
-        font-weight: bold;
-        margin-bottom: 20px;
-    }
-</style>
-""", unsafe_allow_html=True)
-
 # Quicksight Link section
-st.sidebar.markdown('<p class="sidebar-header">Quicksight Link</p>', unsafe_allow_html=True)
+st.sidebar.markdown("### Quicksight Link")
 st.sidebar.markdown('''
 <a href="https://us-east-1.quicksight.aws.amazon.com/sn/account/187419755406_SPS/dashboards/19ca18a9-c62b-4d22-94c3-b180f1cd9640/views/c7b9defa-5e1a-46b6-971a-dfecf4e7c45c" target="_blank">
     <button style="
@@ -27,15 +16,17 @@ st.sidebar.markdown('''
         text-decoration: none;
         display: inline-block;
         font-size: 16px;
+        margin: 4px 2px;
         cursor: pointer;
         border-radius: 4px;
+        transition: background-color 0.3s;
         width: 100%;
     ">Shrinkage and Occupancy Dashboard</button>
 </a>
 ''', unsafe_allow_html=True)
 
 # Data Upload section
-st.sidebar.markdown('<p class="sidebar-header">Data Upload</p>', unsafe_allow_html=True)
+st.sidebar.markdown("### Data Upload")
 uploaded_files = st.sidebar.file_uploader(
     "",
     accept_multiple_files=True,
