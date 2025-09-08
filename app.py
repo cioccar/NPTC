a774c651abc00112fd4f82efde1cbdf23355e95b
cioccar <cioccar@amazon.com>
2025-09-08 17:07:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.sidebar.markdown('''
 # Data Upload section
 st.sidebar.markdown("### Data Upload")
 uploaded_files = st.sidebar.file_uploader(
-    "",
+    "From above dashboard",
     accept_multiple_files=True,
     type=['csv']
 )
