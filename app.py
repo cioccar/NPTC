358b4f4c5b3ae0682e8a227fae6181bffc3ea239
cioccar <cioccar@amazon.com>
2025-09-10 17:20:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,46 +22,48 @@ st.sidebar.markdown("""
 
 # Quicksight Link section
 st.sidebar.markdown('<p class="sidebar-header">Quicksight Links</p>', unsafe_allow_html=True)
-st.sidebar.markdown('n('''
-<a href="https://us-east-1.quicksight.aws.amazon.com/sn/account/187419755406_SPS/dashboards/19ca18a9-c62b-4d22-94c3-b180f1cd9640/views/07e65948-7d03-4795-88e5-76143c5c09ce">
-    <button style="
-        background-color: white; 
-        border: 1px solid #cccccc;
-        color: black;
-        padding: 10px 24px;
-        text-align: center;
-        text-decoration: none;
-        displaylay: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        cursor: pointer;
-        border-radius: 4px;
-        transition: backgckground-color 0.3s;
-        width: 100%;
-    ">Deep Dive Dashboard</button>
-</a>
-''', unsafe_al_allow_html=True)
+st.sidebar.markdown(
+    '<a href="https://us-east-1.quicksight.aws.amazon.com/sn/account/187419755406_SPS/dashboards/19ca18a9-c62b-4d22-94c3-b180f1cd9640/views/07e65948-7d03-4795-88e5-76143c5c09ce">'
+    '<button style="'
+    'background-color: white;'
+    'border: 1px solid #cccccc;'
+    'color: black;'
+    'padding: 10px 24px;'
+    'text-align: center;'
+    'text-decoration: none;'
+    'display: inline-block;'
+    'font-size: 16px;'
+    'margin: 4px 2px;'
+    'cursor: pointer;'
+    'border-radius: 4px;'
+    'transition: background-color 0.3s;'
+    'width: 100%;">'
+    'Deep Dive Dashboard</button>'
+    '</a>',
+    unsafe_allow_html=True
+)
 
 # Add Occupancy WBR Dashboard button
-st.sidebar.markdown('''
-<a href="https://us-east-1.quicksight.aws.amazon.com/sn/account/187419755406_SPS/dashboards/19ca1ca18a9-c62b-4d22-94c3-b180f1cd9640/views/e7c15434-94c8-40a8-aafe-15b6be3c68da" target="_blank">
-    <button style="
-        background-color: white; 
-        border: 1px solid #cccccc;
-        color: black;
-        padding: 10px 24px;
-        text-align: n: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        cursor: pointer;
-        border-radius: 4px;
-        transition: background-color 0.3s;
-        width: 100%;
-    ">Occupancy WBR Dashboard</button>
-</a>
-''', unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<a href="https://us-east-1.quicksight.aws.amazon.com/sn/account/187419755406_SPS/dashboards/19ca18a9-c62b-4d22-94c3-b180f1cd9640/views/e7c15434-94c8-40a8-aafe-15b6be3c68da" target="_blank">'
+    '<button style="'
+    'background-color: white;'
+    'border: 1px solid #cccccc;'
+    'color: black;'
+    'padding: 10px 24px;'
+    'text-align: center;'
+    'text-decoration: none;'
+    'display: inline-block;'
+    'font-size: 16px;'
+    'margin: 4px 2px;'
+    'cursor: pointer;'
+    'border-radius: 4px;'
+    'transition: background-color 0.3s;'
+    'width: 100%;">'
+    'Occupancy WBR Dashboard</button>'
+    '</a>',
+    unsafe_allow_html=True
+)
 
 # Data Upload section with reduced spacing
 st.sidebar.markdown('<p class="sidebar-header">Data Upload</p>', unsafe_allow_html=True)
