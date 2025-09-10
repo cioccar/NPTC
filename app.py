c535895cbca38bbaa95d76a37176ca8db4a8a305
cioccar <cioccar@amazon.com>
2025-09-10 18:21:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.sidebar.markdown("""
 """, unsafe_allow_html=True)
 
 # Quicksight Link section
-st.sidebar.markdown('<p class="sidebar-header">Quicksight Links</p>', unsafe_allow_html=TruTrue)
+st.sidebar.markdown('<p class="sidebar-header">Quicksight Links</p>', unsafe_allow_html=True) 
 st.sidebar.markdown(
     f'''
     <a href="https://us-east-1.quicksight.aws.amazon.com/sn/account/187419755406_SPS/dashboards/19ca18a9-c62b-4d22-94c3-b180f1cd9640/views/07e65948-7d03-4795-88e5-76143c5c09ce">
